9828e3f88d73e02d946ee81daa38c65b8fc1d81f
Ruslan <yaarusik@gmail.com>
2021-08-04 12:26:26 +0500

    fix: code refactoring

--- src/index.js
+++ src/index.js
@@ -1,43 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-    let brackets = [];
-
-    for (let i = 0; i < bracketsConfig.length; i++) {
-        brackets.push(bracketsConfig[i][0]);
-    }
-
     const bracketsPair = {};
 
     for (let j = 0; j < bracketsConfig.length; j++) {
-        bracketsPair[bracketsConfig[j][1]] = bracketsConfig[j][0];
+        bracketsPair[bracketsConfig[j][0]] = bracketsConfig[j][1];
     }
 
     let stack = [];
 
     for (let i = 0; i < str.length; i++) {
         let currentBracket = str[i];
-        if (brackets.includes(currentBracket)) {
-            stack.push(currentBracket);
-        } else if (stack.length === 0) {
-            return false;
-        }
 
         topElement = stack[stack.length - 1];
 
-        if (bracketsPair[currentBracket] == topElement) {
+        if (currentBracket === bracketsPair[topElement]) {
             stack.pop();
-        } else if (stack.length === 0) {
-            // return false;
+        } else if (bracketsPair[currentBracket]) {
+            stack.push(currentBracket);
+        } else {
+            return false;
         }
     }
     return stack.length == 0;
 };
-
-/*console.log(
-    check("|(|)", [
-        ["(", ")"],
-        ["|", "|"],
-    ])
-);*/
-
-//const config1 = [["(", ")"]];
-//check("()", config1), true;
